7297a42f323392cf797dd84e35dd3bf11f7cdcbb
strukfit <strukfit0@gmail.com>
2023-10-29 19:37:36 +0100

    Add project files.

---
+++ CourseProject/CourseProject.cpp
@@ -0,0 +1,12 @@
+﻿// CourseProject.cpp : Defines the entry point for the application.
+//
+
+#include "CourseProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
